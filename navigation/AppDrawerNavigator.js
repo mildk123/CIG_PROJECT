a5a92cee729074e42f87f1225de723cf153a5356
mildk123 <mildk123@gmail.com>
2019-01-08 17:04:38 +0500

    Drawer #1

--- navigation/AppDrawerNavigator.js
+++ navigation/AppDrawerNavigator.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { ScrollView, Text } from 'react-native'
+
 import { createDrawerNavigator } from "react-navigation";
 
 // import MainAppNav from './MainAppNav'
-import HomeStackNavigator from '../Screens/Homescreen'
-import AddProduct from '../Screens/AddProduct'
+import HomeStackNavigator from "../Screens/Homescreen";
+import AddProduct from "../Screens/AddProduct";
 import SignOut from "../components/Sign Out";
 
-export default createDrawerNavigator({
-  Main : HomeStackNavigator,
-  AddProduct : AddProduct,
-  'Sign out' : SignOut
-});
+import DrawerItems from '../components/sideMenu'
 
-
+export default createDrawerNavigator(
+  {
+    Main: HomeStackNavigator,
+    AddProduct: AddProduct,
+    "Sign out": SignOut
+  },
+  {
+    // define customComponent here
+    contentComponent: props => (
+      <ScrollView>
+        <DrawerItems {...props} />
+        <Text>Your Own Footer Area After</Text>
+      </ScrollView>
+    )
+  }
+);
